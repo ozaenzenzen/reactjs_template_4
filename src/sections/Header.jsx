41b416695c08a45fc02f74cee674dac6fe226c43
Fauzan Akmal Mahdi <fauzan@loyalto.id>
2025-01-25 22:40:29 +0700

    setup tailwind and fiel

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return (
-    <div>
-      Header
-      <div class="scheme-light ...">
-        <input type="date" />
-      </div>
-      <div class="scheme-dark ...">
-        <input type="date" />
-      </div>
-      <div class="scheme-light-dark ...">
-        <input type="date" />
-      </div>
-    </div>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        themeblack: "#202020",
+        themeblack: "#f50e32",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
