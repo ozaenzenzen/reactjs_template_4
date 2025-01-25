40d7d20ca9058cf01b4f7a2ba445cef8bad38ef6
Fauzan Akmal Mahdi <fauzan@loyalto.id>
2025-01-25 22:31:06 +0700

    setup tailwind

---
+++ src/sections/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      Header
+      <div class="scheme-light ...">
+        <input type="date" />
+      </div>
+      <div class="scheme-dark ...">
+        <input type="date" />
+      </div>
+      <div class="scheme-light-dark ...">
+        <input type="date" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
