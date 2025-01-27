6e6186530cb3eb861014d75f067c5be7cf9095a8
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-27 18:49:06 +0700

    update footer section

--- src/sections/Cta.jsx
+++ src/sections/Cta.jsx
@@ -14,7 +14,7 @@ const Cta = () => {
   }, []);
 
   return (
-    <div className="w-full lg:px-[200px] px-8 py-[60px] h-full flex flex-col justify-between items-center gap-5">
+    <div id="cta" className="w-full lg:px-[200px] px-8 py-[60px] h-full flex flex-col justify-between items-center gap-5">
       <div className="w-full bg-[#2f2f2f] flex lg:flex-row flex-col justify-between items-center lg:p-20 p-8">
         <h1 className="text-white lg:w-[50%] w-full lg:text-[35px] text-[30px] leading-10 lg:text-left text-center font-semibold font-poppins">
           Discorver How Design Can Help Your Business
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { FaFacebookF } from 'react-icons/fa';
+import { RiInstagramFill } from 'react-icons/ri';
+import { FaTwitter, FaLinkedinIn, FaArrowUp } from 'react-icons/fa';
+import { Link } from 'react-scroll';
 
 const Footer = () => {
+  useEffect(() => {
+      AOS.init({
+        duration: 800,
+        delay: 200,
+        once: true,
+      });
+    }, []);
+
   return (
-    <div>Footer</div>
+    <div id='contact'>Footer</div>
   )
 }
 
